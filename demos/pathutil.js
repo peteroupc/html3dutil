46c6864caaef1dc98b46ce697ce10562fc316a96
Peter Occil <poccil14@gmail.com>
2024-08-08 10:31:45 -0400

    Nonsubstantive code edits

--- demos/pathutil.js
+++ demos/pathutil.js
@@ -10,6 +10,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 /* global H3DU */
 /*
  Any copyright to this file is released to the Public Domain.
--- extern.js
+++ extern.js
@@ -14,5 +14,9 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 /* exported H3DU, module, exports, define */
 var module, exports, define;
