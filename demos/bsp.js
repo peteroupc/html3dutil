5e470855e5fa763e5b20ff9ea3b2ed0c175a16be
Peter Occil <poccil14@gmail.com>
2024-08-09 09:58:20 -0400

    Nonsubstantive code edits

--- demos/bsp.js
+++ demos/bsp.js
@@ -14,6 +14,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 /* global AABBTree, H3DU, MeshBuffer, classifyPointToPlane3D, classifyPolygonToPlane3D, getIntersectionRayPlane, makeRay, triangleToPlane */
 /*
  Any copyright to this file is released to the Public Domain.
