1028073de1ce262a1d07724cb28684d9684565b4
Peter Occil <poccil14@gmail.com>
2024-08-10 01:50:00 -0400

    Nonsubstantive code edits

--- promise.js
+++ promise.js
@@ -22,6 +22,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 
 /* eslint no-extend-native: "off", callback-return: "off" */
 // Notes by Peter O.:
