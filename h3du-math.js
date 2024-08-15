d45f885c6d68f1810951e5cea2861ed3e08e54c3
Peter Occil <poccil14@gmail.com>
2024-08-15 13:06:47 -0400

    Nonsubstantive code edits

--- h3du-math.js
+++ h3du-math.js
@@ -58,6 +58,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 
 /**
  * A collection of math functions for working
