a0b5ab366506d7dfd9f0bf19a57a5d1d36acb1a0
Peter Occil <poccil14@gmail.com>
2024-08-15 10:05:04 -0400

    Nonsubstantive code edits

--- h3du-curve.js
+++ h3du-curve.js
@@ -38,6 +38,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 
 import {MathInternal} from "./h3du-mathinternal";
 
