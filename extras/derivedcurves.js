2d600667c4bbe311f76ce7b8b3325b1c99a06244
Peter Occil <poccil14@gmail.com>
2024-08-19 06:42:50 -0400

    Nonsubstantive code edits

--- extras/derivedcurves.js
+++ extras/derivedcurves.js
@@ -58,6 +58,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 /**
  * Additional curve evaluator and surface evaluator objects.<p>
  * To import all symbols in this module, either of the following can be used:
