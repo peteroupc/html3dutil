6a52e6ae0b9a11eb1afc8429fff4a5da3378c251
Peter Occil <poccil14@gmail.com>
2024-08-22 04:36:28 -0400

    Nonsubstantive code edits

--- h3du-shapegroup.js
+++ h3du-shapegroup.js
@@ -66,6 +66,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 
 import {MathUtil} from "./h3du-math";
 import {Transform} from "./h3du-transform";
