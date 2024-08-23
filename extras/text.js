610569716bb3ec615f07850e88ee6580bd95c8b2
Peter Occil <poccil14@gmail.com>
2024-08-23 05:45:25 -0400

    Nonsubstantive code edits

--- extras/text.js
+++ extras/text.js
@@ -66,6 +66,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 /** The <code>extras/text.js</code> module.
  * To import all symbols in this module, either of the following can be used:
  * <pre>
