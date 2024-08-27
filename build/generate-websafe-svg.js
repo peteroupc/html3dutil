e67cca902ddc1af31398ea1f4024c012e9f38d71
Peter Occil <poccil14@gmail.com>
2024-08-27 12:00:58 -0400

    Nonsubstantive code edits

--- build/generate-websafe-svg.js
+++ build/generate-websafe-svg.js
@@ -74,6 +74,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 /* global exports */
 /*
  Any copyright to this file is released to the Public Domain.
--- extras/randompolygon.js
+++ extras/randompolygon.js
@@ -74,6 +74,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 /** Contains a method to generate a simple polygon at random.<p>
  * To import all symbols in this module, either of the following can be used:
  * <pre>
