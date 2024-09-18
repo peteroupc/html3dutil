0997f22982197a1eaf37bf81e2ceeeeadac47cf3
Peter Occil <poccil14@gmail.com>
2024-09-18 15:10:04 -0400

    Nonsubstantive code edits

--- demos/implicit.js
+++ demos/implicit.js
@@ -86,6 +86,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 /* global H3DU */
 // TODO: Convert this code to use MeshBuffer instead of Mesh
 // The following was adapted by Peter O. from public-domain
--- h3du-piecewisecurve.js
+++ h3du-piecewisecurve.js
@@ -86,6 +86,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 
 import {BSplineCurve} from "./h3du-bspline";
 import {Curve} from "./h3du-curve";
