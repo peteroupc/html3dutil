decbcf9edf5536e6f54af7fb9dc0d42a0f6e6431
Peter Occil <poccil14@gmail.com>
2024-09-27 23:32:08 -0400

    Nonsubstantive code edits; etc.

--- extern.js
+++ extern.js
@@ -94,5 +94,17 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 /* exported H3DU, module, exports, define */
 var module, exports, define;
