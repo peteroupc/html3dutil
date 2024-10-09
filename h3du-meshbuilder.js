52444398961298361e5848840da190e633d970d9
Peter Occil <poccil14@gmail.com>
2024-10-09 00:07:50 -0400

    Nonsubstantive code edits

--- h3du-meshbuilder.js
+++ h3du-meshbuilder.js
@@ -142,6 +142,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 /* global Uint16Array, Uint32Array */
 /*
  Any copyright to this file is released to the Public Domain.
