72efb575e5e890f480b48ee95d1fefc9cb32dd64
Peter Occil <poccil14@gmail.com>
2024-10-10 05:51:48 -0400

    Nonsubstantive code edits

--- demos/demoutil.js
+++ demos/demoutil.js
@@ -150,6 +150,10 @@
  Any copyright to this file is released to the Public Domain.
  In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
 */
+/*
+ Any copyright to this file is released to the Public Domain.
+ In case this is not possible, this file is also licensed under the Unlicense: https://unlicense.org/
+*/
 /* global H3DU, alert, updateShape */
 /* exported formulaEditorHelp */
 function formulaEditorHelp() {
